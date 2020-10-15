2618fc5f217a734f2bcc634322970c0f45cba779
Dodis <dav3.bjorklund@gmail.com>
2020-10-15 13:06:32 +0300

    fixade uppg16

--- uppg16.java
+++ uppg16.java
@@ -14,6 +14,7 @@ public class uppg16 {
      
         System.out.println("\nDitt användarnamn: " + getUsername(name));
     }
+// printar alla namn från en array
 
     static void printAllNames (String name) {
         String[] splitArr = name.split(" ", 5 );
@@ -24,26 +25,36 @@ public class uppg16 {
 
     }
 
+    //printar ett username från 2 av förnamn bokstäver och av efternamn om det finns 7 bokstäver 
     static String getUsername (String name) {
-        String userName = "";
+        String userName;
 
         
         String[] splitArr = name.split(" ", 5);
         if (splitArr[0].length() > 1) {
-            userName = userName + splitArr[0].substring(0,1);
+            userName = splitArr[0].substring(0,1);
         } else {
             userName = splitArr[0];
         }
         
-        if (splitArr[splitArr.length-1].length() > 7 ) {
+        if (splitArr[splitArr.length-1].length() >= 7) {
             userName = userName + splitArr[splitArr.length-1].substring(0, 7);
         } else {
-            userName += splitArr[splitArr.length-1];
+            userName = userName + splitArr[splitArr.length-1];
         }
        
-        
-
-        return userName;
+        return nordicEraser(userName);
     }
 
+    //metod som byter ut å ä ö
+    static String nordicEraser (String name) {
+        String newName = name.replaceAll("ö", "o");
+        newName = newName.replaceAll("Ö", "O");
+        newName = newName.replaceAll("ä", "a");
+        newName = newName.replaceAll("Ä", "A");
+        newName = newName.replaceAll("å", "a");
+        newName = newName.replaceAll("Å", "A");        
+        
+        return newName;
+    }
 }
